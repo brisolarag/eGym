cb4fee26c08eff65337efa93c0c4b761b92f3d0f
Gabriel Brisolara <dev.brisolara@gmail.com>
2024-07-11 17:10:25 -0300

    first layout defined

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { OverviewComponent } from './components/content/overview/overview.component';
+import { NotFoundComponent } from './components/content/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = 
+[
+    {
+        path: 'overview',
+        component: OverviewComponent
+    },
+
+    {
+        path: '',
+        redirectTo: 'overview',
+        pathMatch: 'full'
+    },
+
+    {
+        path: '**',
+        component: NotFoundComponent
+    }
+];
